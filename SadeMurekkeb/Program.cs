75f1919a53996503f19a135e31f19eca075192cc
Nurlan <nurlanea@code.edu.az>
2023-10-31 20:21:28 +0400

    upload

---
+++ SadeMurekkeb/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine(num(11));
+bool num (int n)
+{
+    int result = 0;
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            result++;
+            if (result > 1)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
